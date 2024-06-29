a7fe217234d7c9169484b0652c71f78e97822b6d
Web Diary Academy Official <webdiaryacademy@gmail.com>
2024-06-30 01:17:56 +0400

    added more comments for flatten deeply nested array

--- 2625. Flatten Deeply Nested Array/main.js
+++ 2625. Flatten Deeply Nested Array/main.js
@@ -1,14 +1,32 @@
-let arr = [1, 2, [7, 8, [9, 10, 11], 12], [13, 14, 15]];
-let n = 1;
+// n = 0
+// Output
+// [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]]
 
+// n = 1
+// Output
+// [1, 2, 3, 4, 5, 6, 7, 8, [9, 10, 11], 12, 13, 14, 15]
+
+// n = 2
+// Output
+// [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+
+let arr = [1, 2, 3, [4, 5, 6], [7, 8, [9, 10, 11], 12], [13, 14, 15]];
+let n = 2;
 let flattened = [];
 
 const flat = function (arr, n) {
-  arr.forEach((item) => {
-    if (Array.isArray(item) && n > 0) {
-      flat(item, n - 1);
+  arr.forEach((element) => {
+    if (Array.isArray(element) && n > 0) {
+      // Means: Found an inner array.
+      // Now what, think twice before you write next line.
+      // Ah okay, I need to flatten the array.
+      // Let me flatten.
+      // Wait wait, the element which I got is array of array [7, 8, [9, 10, 11], 12], shit
+      // I got the idea, let me use the recurssion here
+      flat(element, n - 1);
     } else {
-      flattened.push(item);
+      // Means: The element is a number, dont think twice, push it
+      flattened.push(element);
     }
   });
 
