b6dade4644d7697b067b12420f973fc06028711e
Web Diary Academy Official <webdiaryacademy@gmail.com>
2024-07-14 03:01:46 +0400

    feat: move zeros solution

---
+++ 283. Move Zeroes/main.js
@@ -0,0 +1,24 @@
+
+let nums = [0,1,0,3,12]
+// Output: [1,3,12,0,0]
+
+const moveZeroes = function(nums) {
+    let numsLength = nums.length
+
+    while(0 < numsLength) {
+      for(let i=0; i<numsLength -1; i++) {
+        if(nums[i] === 0){
+          let temp = nums[i+1]
+          nums[i+1] = 0
+          nums[i] = temp
+        }
+      }
+
+      numsLength--
+    }
+
+    return nums
+
+};
+
+console.log(moveZeroes(nums))
